ad10bbe777dd0d5f05ddd5336b412dac436a5dee
Alexandra <aleksandrakurilo61@gmail.com>
2026-03-24 12:54:52 +0200

    react-FAQ reusable button component

--- react-FAQ/src/components/Button/Button.tsx
+++ react-FAQ/src/components/Button/Button.tsx
@@ -1,12 +1,22 @@
 import type { FC, ReactNode } from "react";
+import { Link } from "react-router-dom";
 import cls from "./Button.module.css";
 
 interface ButtonProps {
   children: ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
+  to?: string;
 }
 
-export const Button: FC<ButtonProps> = ({ onClick, children }) => {
+export const Button: FC<ButtonProps> = ({ onClick, children, to }) => {
+  if (to) {
+    return (
+      <Link className={cls.viewMoreBtn} to={to}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button className={cls.viewMoreBtn} onClick={onClick} type="button">
       {children}
